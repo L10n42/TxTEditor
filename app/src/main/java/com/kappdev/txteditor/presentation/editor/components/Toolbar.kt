d1fe5a57595dab339e2af7ab5457e56da50269cd
Kolya <kola13r@gmail.com>
2023-10-16 10:12:28 +0300

    commit 7 (3d toolbar effect added)

--- app/src/main/java/com/kappdev/txteditor/presentation/editor/components/Toolbar.kt
+++ app/src/main/java/com/kappdev/txteditor/presentation/editor/components/Toolbar.kt
@@ -18,25 +18,40 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.kappdev.txteditor.presentation.editor.ToolbarAction
-import com.kappdev.txteditor.ui.theme.AppTheme
 
 @Composable
 fun Toolbar(
     onAction: (ToolbarAction) -> Unit,
 ) {
+    val color = MaterialTheme.colorScheme.surface
+    val shadowColor = MaterialTheme.colorScheme.onSurface
+
     Surface(
         shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
-        color = MaterialTheme.colorScheme.surface,
+        color = Color.Transparent,
         shadowElevation = 6.dp
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(48.dp),
+                .height(48.dp)
+                .drawBehind {
+                    toolbarForm(
+                        background = color,
+                        shadowColor = shadowColor.copy(0.24f)
+                    )
+                },
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceAround
         ) {
@@ -57,6 +72,100 @@ fun Toolbar(
             }
         }
     }
+}
+
+private fun DrawScope.toolbarForm(
+    background: Color,
+    border: Dp = 8.dp,
+    shadowColor: Color
+) {
+    val width = this.size.width
+    val height = this.size.height
+    val cornerSize = (height / 3)
+    val borderWidth = border.toPx() / width
+    val borderHeight = border.toPx() / height
+    val cornerBorder = border.toPx() / cornerSize
+
+    val formPath = Path().apply {
+        moveTo(0f, height)
+        lineTo(0f, cornerSize)
+        quadraticBezierTo(
+            x1 = 0f,
+            y1 = 0f,
+            x2 = cornerSize,
+            y2 = 0f
+        )
+        lineTo(width - cornerSize ,0f)
+        quadraticBezierTo(
+            x1 = width,
+            y1 = 0f,
+            x2 = width,
+            y2 = cornerSize
+        )
+        lineTo(width, height)
+        close()
+    }
+
+    drawPath(
+        path = formPath,
+        color = background
+    )
+    drawPath(
+        path = formPath,
+        color = shadowColor
+    )
+
+    drawRect(
+        brush = Brush.verticalGradient(
+            (1f - borderHeight) to background,
+            1f to Color.Transparent,
+            startY = height,
+            endY = 0f
+        ),
+        topLeft = Offset(cornerSize, 0f),
+        size = Size(width - (2 * cornerSize), height)
+    )
+
+    drawRect(
+        brush = Brush.horizontalGradient(
+            0f to Color.Transparent,
+            borderWidth to background,
+            (1f - borderWidth) to background,
+            1f to Color.Transparent,
+            startX = 0f,
+            endX = width
+        ),
+        topLeft = Offset(0f, cornerSize),
+        size = Size(width, height - cornerSize)
+    )
+
+    drawArc(
+        brush = Brush.radialGradient(
+            (1f - cornerBorder) to background,
+            1f to Color.Transparent,
+            radius = cornerSize,
+            center = Offset(cornerSize, cornerSize)
+        ),
+        startAngle = -90f,
+        sweepAngle = -90f,
+        useCenter = true,
+        topLeft = Offset.Zero,
+        size = Size(cornerSize * 2, cornerSize * 2)
+    )
+
+    drawArc(
+        brush = Brush.radialGradient(
+            (1f - cornerBorder) to background,
+            1f to Color.Transparent,
+            radius = cornerSize,
+            center = Offset(width - cornerSize, cornerSize)
+        ),
+        startAngle = -90f,
+        sweepAngle = 90f,
+        useCenter = true,
+        topLeft = Offset(width - (cornerSize * 2), 0f),
+        size = Size(cornerSize * 2, cornerSize * 2)
+    )
 }
 
 @Composable
